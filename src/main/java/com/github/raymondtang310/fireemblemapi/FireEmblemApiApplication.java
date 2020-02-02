e126bdc001458758bdd8fc24caa6ecd43b3bf986
Raymond <raymondtang310@gmail.com>
2020-02-01 20:00:12 -0500

    Load dotenv for local environment only

--- src/main/java/com/github/raymondtang310/fireemblemapi/FireEmblemApiApplication.java
+++ src/main/java/com/github/raymondtang310/fireemblemapi/FireEmblemApiApplication.java
@@ -2,6 +2,7 @@ package com.github.raymondtang310.fireemblemapi;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.MapPropertySource;
@@ -15,20 +16,25 @@ import java.util.stream.Collectors;
 public class FireEmblemApiApplication {
 
     public static void main(String[] args) {
-        Map<String, Object> env = Dotenv.load()
-                .entries()
-                .stream()
-                .collect(
-                        Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+        // Set environment variables from dotenv file for local environment
+        if (System.getenv("APP_ENV") == null) {
+            Map<String, Object> env = Dotenv.load()
+                    .entries()
+                    .stream()
+                    .collect(
+                            Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
 
-        new SpringApplicationBuilder(FireEmblemApiApplication.class)
-                .environment(new StandardEnvironment() {
-                    @Override
-                    protected void customizePropertySources(MutablePropertySources propertySources) {
-                        super.customizePropertySources(propertySources);
-                        propertySources.addLast(new MapPropertySource("dotenvProperties", env));
-                    }
-                }).run(args);
+            new SpringApplicationBuilder(FireEmblemApiApplication.class)
+                    .environment(new StandardEnvironment() {
+                        @Override
+                        protected void customizePropertySources(MutablePropertySources propertySources) {
+                            super.customizePropertySources(propertySources);
+                            propertySources.addLast(new MapPropertySource("dotenvProperties", env));
+                        }
+                    }).run(args);
+        } else {
+            SpringApplication.run(FireEmblemApiApplication.class, args);
+        }
     }
 
 }
--- src/test/java/com/github/raymondtang310/fireemblemapi/FireEmblemApiApplicationTest.java
+++ src/test/java/com/github/raymondtang310/fireemblemapi/FireEmblemApiApplicationTest.java
@@ -4,33 +4,58 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.NoMoreInteractions;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Dotenv.class, FireEmblemApiApplication.class})
+@PrepareForTest({Dotenv.class, SpringApplication.class, FireEmblemApiApplication.class})
 public class FireEmblemApiApplicationTest {
 
     @Test
-    public void testMainShouldLoadDotenv() throws Exception {
+    public void testMainShouldLoadDotenvInLocalEnvironment() throws Exception {
         String[] args = {};
-        PowerMockito.spy(Dotenv.class);
+        PowerMockito.mockStatic(System.class);
+        PowerMockito.when(System.getenv("APP_ENV")).thenReturn(null);
+        Dotenv dotenvMock = PowerMockito.mock(Dotenv.class);
+        PowerMockito.mockStatic(Dotenv.class);
+        PowerMockito.when(Dotenv.load()).thenReturn(dotenvMock);
         SpringApplicationBuilder springApplicationBuilderMock = PowerMockito.mock(SpringApplicationBuilder.class);
         PowerMockito.whenNew(SpringApplicationBuilder.class).withArguments(Mockito.any(Class.class)).thenReturn(springApplicationBuilderMock);
         PowerMockito.doReturn(springApplicationBuilderMock).when(springApplicationBuilderMock).environment(Mockito.any(ConfigurableEnvironment.class));
 
         FireEmblemApiApplication.main(args);
 
-        PowerMockito.verifyStatic(Dotenv.class);
+        PowerMockito.verifyStatic(Dotenv.class, Mockito.times(1));
         Dotenv.load();
     }
 
     @Test
-    public void testMainShouldRunSpringApplication() throws Exception {
+    public void testMainShouldNotLoadDotenvInDeplyoyedEnvironment() {
         String[] args = {};
+        PowerMockito.mockStatic(System.class);
+        PowerMockito.when(System.getenv("APP_ENV")).thenReturn("production");
+        PowerMockito.mockStatic(Dotenv.class);
+        PowerMockito.mockStatic(SpringApplication.class);
+
+        FireEmblemApiApplication.main(args);
+
+        PowerMockito.verifyStatic(Dotenv.class, new NoMoreInteractions());
+        Dotenv.load();
+    }
+
+    @Test
+    public void testMainShouldRunSpringApplicationBuilderInLocalEnvironment() throws Exception {
+        String[] args = {};
+        PowerMockito.mockStatic(System.class);
+        PowerMockito.when(System.getenv("APP_ENV")).thenReturn(null);
+        Dotenv dotenvMock = PowerMockito.mock(Dotenv.class);
+        PowerMockito.mockStatic(Dotenv.class);
+        PowerMockito.when(Dotenv.load()).thenReturn(dotenvMock);
         SpringApplicationBuilder springApplicationBuilderMock = PowerMockito.mock(SpringApplicationBuilder.class);
         PowerMockito.whenNew(SpringApplicationBuilder.class).withArguments(Mockito.any(Class.class)).thenReturn(springApplicationBuilderMock);
         PowerMockito.doReturn(springApplicationBuilderMock).when(springApplicationBuilderMock).environment(Mockito.any(ConfigurableEnvironment.class));
@@ -38,5 +63,18 @@ public class FireEmblemApiApplicationTest {
         FireEmblemApiApplication.main(args);
 
         Mockito.verify(springApplicationBuilderMock, Mockito.times(1)).run(args);
+    }
+
+    @Test
+    public void testMainShouldRunSpringApplicationInDeployedEnvironment() {
+        String[] args = {};
+        PowerMockito.mockStatic(System.class);
+        PowerMockito.when(System.getenv("APP_ENV")).thenReturn("production");
+        PowerMockito.mockStatic(SpringApplication.class);
+
+        FireEmblemApiApplication.main(args);
+
+        PowerMockito.verifyStatic(SpringApplication.class, Mockito.times(1));
+        SpringApplication.run(FireEmblemApiApplication.class, args);
     }
 }
